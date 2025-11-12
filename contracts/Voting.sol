06a4abf2c92da2d11d59c4ff1d14ede6fc586b9a
M Daffa A <daffa.alghifary62@gmail.com>
2025-11-12 19:55:45 +0700

    feat(smart contract): adjusting logics

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -2,31 +2,31 @@
 pragma solidity ^0.8.28;
 
 contract Voting {
+    enum VotingState { Pending, Active, Closed }
+    VotingState public state;
     address public admin;
-    bool voteStart;
     
     struct Voter {
-        address voter;
         uint votedCandidate;
+        string name;
         bool hasVoted;   
         bool isRegistered;
     }
 
     struct Candidate {
-        address candidateAddr;
         uint candidateID;
+        string name;
         uint totalVote;
-        bool isCandidate;
     }
 
-    uint public ID;
     mapping(address => Voter) public voters;
     mapping(address => Candidate) public candidates;
 
-    Candidate[] public candidateList;
+    address[] public candidateAddresses;
 
     constructor() {
         admin = msg.sender;
+        state = VotingState.Pending;
     }
 
     modifier onlyAdmin {
@@ -34,53 +34,52 @@ contract Voting {
         _;
     }
 
-    event registerVoterSuccess(address indexed admin, address indexed voter);
-    event registerCandidateSuccess(address indexed admin, address indexed candidate);
+    event registerVoterSuccess(address indexed admin, address indexed voter, string name);
+    event registerCandidateSuccess(address indexed admin, address indexed candidate, string name);
     event castVoteSuccess(address indexed voter, uint indexed candidateVoted);
     event VotingStarted(address indexed admin);
     event VotingEnded(address indexed admin);
 
-    function registerVoter(address _voterAdd) external onlyAdmin {
-        require(!voteStart, "Vote already start!");
+    function registerVoter(address _voterAdd, string memory _name) external onlyAdmin {
+        require(state == VotingState.Pending, "Vote already start!");
         require(!voters[_voterAdd].isRegistered, "Voter already registered!");
         require(_voterAdd != address(0), "Address invalid!");
 
         voters[_voterAdd] = Voter({
-            voter: _voterAdd,
             votedCandidate: 0,
+            name: _name,
             hasVoted: false,
             isRegistered: true
         });
 
-        emit registerVoterSuccess(msg.sender, _voterAdd);
+        emit registerVoterSuccess(msg.sender, _voterAdd, _name);
     }
 
-    function registerCandidate(address _candidateAdd) external onlyAdmin {
-        require(!voteStart, "Vote already start!");
-        require(!candidates[_candidateAdd].isCandidate, "Candidate already registered!");
+    function registerCandidate(address _candidateAdd, string memory _name) external onlyAdmin {
+        require(state == VotingState.Pending, "Vote already start!");
+        require(bytes(candidates[_candidateAdd].name).length == 0, "Candidate already registered!");
         require(!voters[_candidateAdd].isRegistered, "Voter cannot be a candidate!");
         require(_candidateAdd != address(0), "Address invalid!");
 
-        uint newID = ID++;
+        uint newID = candidateAddresses.length;
         candidates[_candidateAdd] = Candidate({
-            candidateAddr: _candidateAdd,
             candidateID: newID,
-            totalVote: 0,
-            isCandidate: true
+            name: _name,
+            totalVote: 0
         });
 
-        candidateList.push(candidates[_candidateAdd]);
+        candidateAddresses.push(_candidateAdd);
 
-        emit registerCandidateSuccess(msg.sender, _candidateAdd);
+        emit registerCandidateSuccess(msg.sender, _candidateAdd, _name);
     }
 
     function castVote(uint candidateId) external {
         require(voters[msg.sender].isRegistered, "You are not registered!");
-        require(voteStart, "Vote not started!");
+        require(state == VotingState.Active, "Voting not active!");
         require(!voters[msg.sender].hasVoted, "You already vote!");
-        require(candidateId < candidateList.length, "Invalid ID!");
+        require(candidateId < candidateAddresses.length, "Invalid ID!");
 
-        address votedAddr = candidateList[candidateId].candidateAddr;
+        address votedAddr = candidateAddresses[candidateId];
         candidates[votedAddr].totalVote++;
         voters[msg.sender].votedCandidate = candidateId;
         voters[msg.sender].hasVoted = true;
@@ -89,48 +88,47 @@ contract Voting {
     }
 
     function startVoting() external onlyAdmin {
-        require(!voteStart, "Voting has already started!");
-        voteStart = true;
+        require(state == VotingState.Pending, "Voting already started or closed!");
+        state = VotingState.Active;
 
         emit VotingStarted(msg.sender);
     }
 
     function closeVoting() external onlyAdmin {
-        require(voteStart, "Voting has not started yet!");
-        voteStart = false;
-
+        require(state == VotingState.Active, "Voting not active!");
+        state = VotingState.Closed;
         emit VotingEnded(msg.sender);
     }
 
     function showProgress() external view returns(uint[] memory, uint[] memory) {
-        uint totalCandidates = candidateList.length;
+        uint totalCandidates = candidateAddresses.length;
         uint[] memory candidateIds = new uint[](totalCandidates);
-        address[] memory candidatesAddr = new address[](totalCandidates);
         uint[] memory total = new uint[](totalCandidates);
 
         for(uint i = 0; i < totalCandidates; i++) {
-            candidateIds[i] = candidateList[i].candidateID;
-            candidatesAddr[i] = candidateList[i].candidateAddr;
-            total[i] = candidates[candidatesAddr[i]].totalVote;
+            address candidateAddr = candidateAddresses[i];
+            candidateIds[i] = candidates[candidateAddr].candidateID;
+            total[i] = candidates[candidateAddr].totalVote;
         } 
 
         return (candidateIds, total);
     }
 
-    function showResult() external view returns (uint[] memory, address[] memory, uint[] memory) {
-        require(!voteStart, "Vote still active!");
+    function showResult() external view returns (uint[] memory, string[] memory, uint[] memory) {
+        require(state == VotingState.Closed, "Voting not closed yet!");
         
-        uint totalCandidates = candidateList.length;
+        uint totalCandidates = candidateAddresses.length;
         uint[] memory candidateIds = new uint[](totalCandidates);
-        address[] memory candidateAddr = new address[](totalCandidates);
+        string[] memory candidateNames = new string[](totalCandidates);
         uint[] memory votes = new uint[](totalCandidates);
 
         for(uint i = 0; i < totalCandidates; i++) {
-            candidateIds[i] = candidateList[i].candidateID;
-            candidateAddr[i] = candidateList[i].candidateAddr;
-            votes[i] = candidates[candidateAddr[i]].totalVote;
+            address candidateAddr = candidateAddresses[i];
+            candidateIds[i] = candidates[candidateAddr].candidateID;
+            candidateNames[i] = candidates[candidateAddr].name;
+            votes[i] = candidates[candidateAddr].totalVote;
         }
 
-        return (candidateIds, candidateAddr, votes);
+        return (candidateIds, candidateNames, votes);
     }
 }
